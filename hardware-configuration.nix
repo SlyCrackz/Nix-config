7d6e9a1fc58de8dddf4ca993fddaf8981170a2f4
slycrackz <slycrackz@slycrackz.xyz>
2024-09-15 01:34:37 +0000

    timezone
    adding America/New_York timzeone to hardware config

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -46,6 +46,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  time.timeZone = lib.mkDefault "America/New_York";
   # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 

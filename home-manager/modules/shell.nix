dec8530f586e02487387f52dafa3a9a6b435f25b
slycrackz <slycrackz@slycrackz.xyz>
2024-09-16 01:33:11 -0400

    shell tweaks
    fix
    
    fix
    
    tweak
    
    tweak
    
    tweak
    
    tweaks
    
    tweak
    
    tweak big
    
    tweak
    
    tweak
    
    tweak
    
    tweak
    
    tweak
    
    tweak
    
    tweak
    
    tweak

--- home-manager/modules/shell.nix
+++ home-manager/modules/shell.nix
@@ -7,11 +7,29 @@
       enableCompletion = true;
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
-
-      oh-my-zsh = {
-        enable = true;
-        plugins = [ "git" "thefuck" ];
-        theme = "bira";
+    };
+    starship = {
+      enable = true;
+      settings = {
+        username = {
+          style_user = "blue bold";
+          style_root = "red bold";
+          format = "[$user]($style)";
+          disabled = false;
+          show_always = true;
+        };
+        hostname = {
+          ssh_only = false;
+          ssh_symbol = "🌐";
+          format = "[@](bold red)[$hostname](bold yellow)";
+          trim_at = ".local";
+          disabled = false;
+          };
+        add_newline = false;
+        character = {
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[➜](bold red)";
+        };
       };
     };
   };

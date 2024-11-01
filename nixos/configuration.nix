d31d6aaa97fd24def101dfcf357c1c1015856d59
slycrackz <slycrackz@slycrackz.xyz>
2024-11-01 17:32:52 -0400

    plasma updates

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,11 +10,10 @@
   ];
 
   services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
+  services.displayManager.gdm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
-  plasma-browser-integration
+  environment.plasma6.excludePackages = with pkgs.libsForQt5; [
   konsole
   oxygen
   ];

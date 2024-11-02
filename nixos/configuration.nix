31b8beaba86f28e33fb98539961bad331bf8d03c
slycrackz <slycrackz@slycrackz.xyz>
2024-11-01 22:17:24 -0400

    plasma exclusions

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -17,6 +17,10 @@
   services.desktopManager.plasma6.enable = true;
 
   environment.plasma6.excludePackages = with pkgs.libsForQt5; [
+  kate
+  elisa
+  kwrite
+  okular
   konsole
   oxygen
   ];

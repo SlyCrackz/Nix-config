e0a9d6537d8e301993273cc62c96c5a7d3fe8612
slycrackz <slycrackz@slycrackz.xyz>
2024-11-04 11:26:26 -0500

    gokapi update

--- modules/services/gokapi.nix
+++ modules/services/gokapi.nix
@@ -2,12 +2,11 @@
 { pkgs, ... }:
 
 let
-  gokapiVersion = "v1.9.2";
   gokapiPackage = pkgs.stdenv.mkDerivation rec {
     pname = "gokapi";
-    version = gokapiVersion;
+    version = "v1.9.2";
     src = pkgs.fetchurl {
-      url = "https://github.com/Forceu/Gokapi/releases/download/${gokapiVersion}/gokapi-linux_amd64.zip";
+      url = "https://github.com/Forceu/Gokapi/releases/download/${version}/gokapi-linux_amd64.zip";
       sha256 = "bc79d7548ba6d1230e0038898210941c05ce87ea7b1704b812daefa6278175a2"; # Update with the actual checksum
     };
     nativeBuildInputs = [ pkgs.unzip ];
@@ -30,21 +29,5 @@ in
     home = "/var/lib/gokapi";
     description = "Gokapi Service User";
     group = "gokapi";
-  };
-
-  # Enable Gokapi as a systemd service
-  systemd.services.gokapi = {
-    description = "Gokapi Service";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${gokapiPackage}/bin/gokapi";
-      Restart = "on-failure";
-      User = "gokapi";
-      Environment = [
-        "GOKAPI_PORT=53842"
-        "TZ=America/New_York"
-      ];
-    };
   };
 }

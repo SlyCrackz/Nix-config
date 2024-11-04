7542524fec723bb343ecf0433105b459bb42ab37
slycrackz <slycrackz@slycrackz.xyz>
2024-11-04 11:19:20 -0500

    gokapi zip fix

--- modules/services/gokapi.nix
+++ modules/services/gokapi.nix
@@ -2,17 +2,22 @@
 { pkgs, ... }:
 
 let
+  gokapiVersion = "v1.9.2";
   gokapiPackage = pkgs.stdenv.mkDerivation rec {
     pname = "gokapi";
-    version = "v1.9.2";
+    version = gokapiVersion;
     src = pkgs.fetchurl {
-      url = "https://github.com/Forceu/Gokapi/releases/download/${version}/gokapi-linux_amd64.zip";
-      sha256 = "bc79d7548ba6d1230e0038898210941c05ce87ea7b1704b812daefa6278175a2";
+      url = "https://github.com/Forceu/Gokapi/releases/download/${gokapiVersion}/gokapi-linux_amd64.zip";
+      sha256 = "bc79d7548ba6d1230e0038898210941c05ce87ea7b1704b812daefa6278175a2"; # Update with the actual checksum
     };
-    phases = [ "installPhase" ];
+    nativeBuildInputs = [ pkgs.unzip ];
+    phases = [ "unpackPhase" "installPhase" ];
+    unpackPhase = ''
+      unzip ${src}
+    '';
     installPhase = ''
       mkdir -p $out/bin
-      cp ${src} $out/bin/gokapi
+      mv gokapi-linux_amd64 $out/bin/gokapi
       chmod +x $out/bin/gokapi
     '';
   };

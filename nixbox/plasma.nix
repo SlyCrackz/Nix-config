5470993d07521c18631cec62b4cd55b895d5e036
slycrackz <slycrackz@slycrackz.xyz>
2024-11-04 21:03:00 -0500

    gdm -> sddm
    sddm config
    
    sddm fix
    
    sddm compositor

--- nixbox/plasma.nix
+++ nixbox/plasma.nix
@@ -1,9 +1,11 @@
 { pkgs, ... }:
 
 {
-  services.xserver = {
+  services.xserver.enable = true;
+  services.displayManager.sddm = {
     enable = true;
-    displayManager.gdm.enable = true;
+    wayland.enable = true;
+    wayland.compositor = "kwin";
   };
 
   services.desktopManager.plasma6.enable = true;

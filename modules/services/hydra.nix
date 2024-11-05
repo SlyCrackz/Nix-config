296f21c3617c0547edf37506eff7d290ceed976d
slycrackz <slycrackz@slycrackz.xyz>
2024-11-04 23:52:57 -0500

    hydra notification sender

--- modules/services/hydra.nix
+++ modules/services/hydra.nix
@@ -2,6 +2,7 @@
   services.hydra = {
     enable = true;
     hydraURL = "http://localhost:3000"; # For testing locally within the container
+    notificationSender = "hydra@localhost"; # Basic email setting for notifications
     buildMachinesFiles = []; # Avoids using a non-existent /etc/nix/machines file
     useSubstitutes = true; # Speeds up builds by using binary caches
   };

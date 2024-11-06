dbac1d91bc71585dc01b94379e39941bed661b81
slycrackz <slycrackz@slycrackz.xyz>
2024-11-06 16:32:37 -0500

    mac fixes

--- systems/macbook.nix
+++ systems/macbook.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 
 {
+  # Enables Nix flakes and nix-command support
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
   environment.systemPackages = with pkgs; [
     htop
   ];

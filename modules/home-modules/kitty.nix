ca84af7bf67d49884d1574edfa853438c7a0571c
slycrackz <slycrackz@slycrackz.xyz>
2024-11-06 21:49:50 -0500

    kitty.nix

---
+++ modules/home-modules/kitty.nix
@@ -0,0 +1,29 @@
+{
+  programs.kitty = {
+    enable = true;
+    font.name = "IntoneMono Nerd Font";
+    settings = {
+      extraConfig = ''
+        font_size 11
+        foreground #ebdbb2
+        background #282828
+        color0 #282828
+        color1 #fb4934
+        color2 #b8bb26
+        color3 #d79921
+        color4 #83a598
+        color5 #d3869b
+        color6 #689d6a
+        color7 #ebdbb2
+        color8 #928374
+        color9 #fb4934
+        color10 #b8bb26
+        color11 #d79921
+        color12 #83a598
+        color13 #d3869b
+        color14 #689d6a
+        color15 #fbf1c7
+      '';
+    };
+  };
+}

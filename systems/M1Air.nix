517c9f737346dfceaa30c7e486019d4dff7ebeaa
slycrackz <slycrackz@slycrackz.xyz>
2024-11-06 20:41:13 -0500

    unfree pkgs

--- systems/M1Air.nix
+++ systems/M1Air.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 
 {
+  # Unfree packages
+  nixpkgs.config.allowUnfree = true;
+
   # Enables Nix flakes and nix-command support
   nix.extraOptions = ''
     experimental-features = nix-command flakes

8c8da1b93785af9aa01892a45ca8738e41dbfc4d
slycrackz <slycrackz@slycrackz.xyz>
2024-11-08 00:18:44 -0500

    hydra allow unfree

--- systems/ct-hydra-110.nix
+++ systems/ct-hydra-110.nix
@@ -4,22 +4,23 @@
     hydraURL = "http://localhost:3000"; # For testing locally within the container
     notificationSender = "hydra@localhost"; # Basic email setting for notifications
     buildMachinesFiles = [ ]; # Avoids using a non-existent /etc/nix/machines file
-    useSubstitutes = true; # Speeds up builds by using binary caches
+    useSubstitutes = false; # Speeds up builds by using binary caches
   };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    allow-unfree = true; # Enable unfree packages globally
+    build-cores = 0; # Automatically uses all available cores
+  };
 
   nix.buildMachines = [
     {
-      # Keep the current container hostname
-      hostName = "ct-hydra-110";
+      hostName = "ct-hydra-110"; # Keep the current container hostname
       system = "x86_64-linux"; # Target Linux only
-      maxJobs = 8; # Adjust this to match your CPU core count
+      maxJobs = 12; # Adjust to match your CPU core count
       speedFactor = 1;
       supportedFeatures = [ "kvm" "nixos-test" "big-parallel" "benchmark" ];
     }
   ];
-
-  # Increase parallelism for each individual build
-  nix.settings.build-cores = 0; # Automatically uses all available cores
 }
+

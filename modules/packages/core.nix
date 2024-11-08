b739190f25d8af4363c0b563764647659ba9c396
slycrackz <slycrackz@slycrackz.xyz>
2024-11-08 14:35:19 -0500

    unfree

--- modules/packages/core.nix
+++ modules/packages/core.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 
 {
+  # Unrar
+  nixpkgs.config.allowUnfree = true;
+
   # Define system-wide packages here
   environment.systemPackages = with pkgs; [
     # Core tools for all systems
